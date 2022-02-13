eb4d222cbc021deac931c877d6db4285f67c9e26
Gautam Chandra Saha <devgautam1231@gmail.com>
2022-02-13 18:36:02 +0530

    refactored old solution

--- array/subsets.cpp
+++ array/subsets.cpp
@@ -3,19 +3,25 @@
 class Solution{
 public:
 
- vector<vector<int>> subsets(vector<int>& nums){    
-
- 		//* TC: O(), SC: O() 
- 		vector<vector<int>> ans;
-        int n= nums.size();
+vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
         
-        for(int i=0; i<n; i++){
-            for(int j=1; j<n; j++){
-                ans.push_back({nums[i],nums[j]});
+        ans.push_back({});
+        
+        for(int i=0; i<n; i++)
+        {
+            int sz = ans.size();
+            for(int j=0; j<sz; j++)
+            {
+                vector<int> temp = ans[j];
+                temp.push_back(nums[i]);
+                ans.push_back(temp);
             }
         }
- 		return ans;
- }   
+        
+        return ans;
+    }  
 
 } s;
 
